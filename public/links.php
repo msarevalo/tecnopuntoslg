d7d7855b45dad7a4decb6cd9c596bad267ebc74c
msarevalo <msarevalo@unbosque.edu.co>
2019-01-24 18:56:43 -0500

    cambios

--- public/links.php
+++ public/links.php
@@ -35,7 +35,7 @@ function obtener_estructura_directorios($rutas){
                     $var = str_replace(" ", "%20", $archivo);
                     $host = $_SERVER['HTTP_HOST'];
                     //echo "<td><a href='" . $host . "/" . $ruta . "/" . $var . "' target='_blank'>http://" . $host . "/tecnopuntos/recursos/" . $rutas . "/" .  $var . "</a></td>";
-                    echo "<td><a href='" . $host . "/tecnopuntos/" . $ruta . "/" . $var . "' target='_blank'>http://" . $host . "/tecnopuntos/recursos/" . $rutas . "/" .  $var . "</a></td>";
+                    echo "<td><a href='" . $host . "/" . $ruta . "/" . $var . "' target='_blank'>http://" . $host . "/tecnopuntos/recursos/" . $rutas . "/" .  $var . "</a></td>";
                     echo "</tr>";
                 }
             }
