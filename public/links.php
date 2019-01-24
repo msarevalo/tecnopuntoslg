6e96f6c2b335f060b002f760b066fb5daad11074
msarevalo <msarevalo@unbosque.edu.co>
2019-01-24 18:58:57 -0500

    prueba1

--- public/links.php
+++ public/links.php
@@ -36,7 +36,7 @@ function obtener_estructura_directorios($rutas){
                     $host = $_SERVER['HTTP_HOST'];
                     //echo "<td><a href='" . $host . "/" . $ruta . "/" . $var . "' target='_blank'>http://" . $host . "/tecnopuntos/recursos/" . $rutas . "/" .  $var . "</a></td>";
                     //echo "<td><a href='" . $host . "/" . $ruta . "/" . $var . "' target='_blank'>http://" . $host . "/tecnopuntos/recursos/" . $rutas . "/" .  $var . "</a></td>";
-                    echo $host;
+                    echo $host . "<br>" . $ruta;
                     echo "</tr>";
                 }
             }
